b518d7eeb6e8b3afb6ffaf75a236fea55bb285a4
santusantosh <santoshhakki000@mail.com>
2020-04-27 20:28:07 +0530

    part time wage calculated

--- employeeusecase.java
+++ employeeusecase.java
@@ -1,20 +1,32 @@
 public class EmployeeUseCase{
-	
+
 	public static void main(String[] args){
 		int IS_PRESENT=1;
 		double wagePerHour=20;
-		double fullDay=8;	
+		double fullDay=8;
+		double halfDay=4;
 		double empWage;
 		double empCheck=Math.floor(Math.random() * 10 ) %2;
 		System.out.println("Welcome to the Employee Use Case problem");
 
 		if (empCheck==IS_PRESENT){
-			System.out.println("Employee is present");
-			empWage=wagePerHour*fullDay;
-			System.out.println("Employee wage is: "+ empWage);
+			int IS_FULLTIME=1;
+			double fullTimeCheck=Math.floor(Math.random()*10)%2;
+						System.out.println("Employee is present");
+
+			if (fullTimeCheck==IS_FULLTIME){
+				empWage=wagePerHour*fullDay;
+				System.out.println("Employee works fulltime and Employee wage is: "+ empWage);
+			}
+			else {
+				empWage=wagePerHour*halfDay;
+                                System.out.println("Employee works parttime and Employee wage is: "+ empWage);
+			}
 		}
+
 		else{
 			System.out.println("Employee is absent");
 		}
 	}
 }
+
